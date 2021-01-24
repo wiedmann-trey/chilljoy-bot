2fe94152ef4df5eec8a01819d2cf4588362f0430
Jacob Grinberg <jacob@grinberg.net>
2021-01-24 15:32:22 -0500

    testing task creation

--- tasks.py
+++ tasks.py
@@ -1,10 +1,17 @@
-from datetime import datetime
+import datetime
 
-now = datetime.now()
+now = datetime.datetime.now()
 
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 print(now.second)
+
+def difference(time1, time2):
+    date = datetime.date(1, 1, 1)
+    datetime1 = datetime.datetime.combine(date, time1)
+    datetime2 = datetime.datetime.combine(date, time2)
+    time_elapsed = datetime1 - datetime2
+    return time_elapsed
 
 class TaskScheduler():
     def __init__(self):
@@ -18,3 +25,6 @@ class Task():
     def __init__(self, description, hour, minute):
         self.description = description
         self.time = datetime.time(hour=hour, minute=minute)
+        print(difference(self.time,datetime.datetime.now().time()))
+
+t = Task("Finish Math problems", 20, 30)
